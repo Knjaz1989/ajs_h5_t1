bfb562184914b5003e9abf88c69218b407804e46
Igor Sverchkov <knjaz1989@gmail.com>
2023-12-05 23:17:53 +0300

    create classes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Bowman } from "./js/classes/bowman.js";
+import { Swordsman } from "./js/classes/swordsman.js";
+import { Magician } from "./js/classes/magician.js";
+import { Undead } from "./js/classes/undead.js";
+import { Zombie } from "./js/classes/zombie.js";
+import { Daemon } from "./js/classes/daemon.js";
